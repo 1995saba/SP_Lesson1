aca1a60eb26a3260b4da72d9bf8c116fad558526
Sabina <sabina.orunbaeva@gmail.com>
2018-03-12 21:51:18 +0600

    Add files via upload

---
+++ Lesson1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process process = new Process{
+                StartInfo = new ProcessStartInfo("notepad.exe", @"z:\\MyFile.txt")
+            };
+
+            process.Start();
+            Console.WriteLine("Process started...");
+
+            // установка приоритета процесса
+            process.PriorityClass = ProcessPriorityClass.High;
+
+            Console.WriteLine("Process ID: {0}", process.Id);
+            Console.WriteLine("Machine Name: {0}", process.MachineName);
+
+            process.WaitForExit();
+            Console.WriteLine("Process done");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ProcessKiller/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessViewer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process[] procList = Process.GetProcesses();
+            Console.WriteLine("Количество процессов: {0}", procList.Count());
+            Console.WriteLine("Имя процесса\t\t\t\tID\t\tHandles");
+            foreach (Process proc in procList)
+            {
+                int BasePrior = -1;
+                DateTime dt = DateTime.MinValue;
+                try { BasePrior = proc.BasePriority; }
+                catch(Exception) { }
+                try { dt = proc.StartTime; }
+                catch(Exception) { }
+                Console.WriteLine("{0}\t\t\t\t{1}\t\t{2}\t\t{3}\t\t{4}",
+                    proc.ProcessName, proc.Id, 
+                    proc.HandleCount,
+                    BasePrior, dt);
+            }
+            Console.ReadLine();
+        }
+    }
+}
